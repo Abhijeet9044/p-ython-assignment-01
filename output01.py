feece76d8c3300d10943f3da6f1f238fa865223d
Abhijeet9044 <97189713+Abhijeet9044@users.noreply.github.com>
2022-01-14 17:48:35 +0530

    Add files via upload

---
+++ output01.py
@@ -0,0 +1,44 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================== RESTART: C:/Users/Dell 3515 R5/AppData/Local/Programs/Python/Python310/Abhijeet 21105116.py =====================================
+  1st program 
+
+enter first number: 23
+enter second number: 8
+enter third number: 5
+avg of the given numbers is: 12.0
+ 2nd program 
+
+ Kindly enter the values in dollars.
+enter your gross income 12000
+enter number of dependents3
+the tax comes out to be $ -1400.0 
+
+ 3rd program 
+
+Student = [ SID , Name , Gender , Course , CGPA]
+Kindly enter your SID 21105116
+Kindly enter your name Abhijeet chandra
+Kindly enter your gender 'M' , 'F' , 'U' for male , female , unknown respectively.M
+Kindly enter your course nameEce
+Kindly enter your CGPA 7
+student's data :  [21105116, 'Abhijeet chandra', 'M', 'Ece', 7.0] 
+
+ 4th program 
+
+enter marks of student '1' 7
+enter marks of student '2' 8
+enter marks of student '3' 5
+enter marks of student '4' 9
+enter marks of student '5'6
+marks of 5 students are  [7, 8, 5, 9, 6]
+marks in ascending order  [5, 6, 7, 8, 9]
+marks in descending order  [9, 8, 7, 6, 5] 
+
+5th program 
+
+The given list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+The modified list is color 1 = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The modified list if color 2= ['Red', 'Green', 'White', 'Purple', 'Yellow']
