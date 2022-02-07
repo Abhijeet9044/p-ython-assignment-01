533f5a487393a1ab359da8c0f115f1bc031ca5e4
Abhijeet9044 <97189713+Abhijeet9044@users.noreply.github.com>
2022-02-07 15:02:35 +0530

    Add files via upload

---
+++ Output_03_21105116.py
@@ -0,0 +1,69 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================= RESTART: C:/Users/Dell 3515 R5/AppData/Local/Programs/Python/Python310/Assignment 03_ 21105116 _Abhijeet.py =============================
+Question 1
+Please give an input- I am yash. I am a coader
+Total occurances
+i-2
+am-2
+yash.-1
+a-1
+coader-1
+Question--2
+Enter a year [1800-2025]: 1999
+Enter a month [1-12]: 2
+Input a day [1-31]: 29
+The next date is [dd-mm-yyyy] 1-3-1999.
+Question--3
+Enter the number 1: 7
+Enter the number 2: 9
+Enter the number 3: 2
+Enter the number 4: 5
+[2, 5, 7, 9]
+[(2, 4), (5, 25), (7, 49), (9, 81)]
+Question--4
+Enter your Grade Point: 9
+Your Grade is A
+Your Performance is EXCELLENT
+Question--5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+Question--6
+Enter the number of student's data to be stored : 4
+Enter SID :21105116
+Enter Name :Abhijeet
+Enter SID :21105200
+Enter Name :Yash
+Enter SID :21105118
+Enter Name :Priya
+Enter SID :21105114
+Enter Name :Yashi
+{'21105116': 'Abhijeet', '21105200': 'Yash', '21105118': 'Priya', '21105114': 'Yashi'}
+['Abhijeet', 'Priya', 'Yash', 'Yashi']
+['21105114', '21105116', '21105118', '21105200']
+Enter the SID of student
+21105116
+The student is Abhijeet
+Question--7
+Enter the number of Fibonacci sequence that you want: 7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+the avg of series is  3.5
+Question--8
+Elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+set()
+Elements that are in only one of the three sets: [3, 6, 8, 9, 13, 17]
+Elements that are exactly two of the sets: {1, 2, 4, 5}
+Set of all integers in the range 1 to 10 that are not in set1: [6, 7, 8, 9]
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3:  {10, 7}
