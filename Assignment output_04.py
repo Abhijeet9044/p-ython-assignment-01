924248f886b4b6c3029137ce8109b19cd709525a
Abhijeet9044 <97189713+Abhijeet9044@users.noreply.github.com>
2022-02-27 22:05:12 +0530

    Add files via upload

---
+++ Assignment output_04.py
@@ -0,0 +1,76 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================================= RESTART: C:/Users/Dell 3515 R5/AppData/Local/Programs/Python/Python310/Assignment _04.py ======================================
+[Question 1]
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+[Question.2](Recursive)
+The Pascle's Triangle using recursive approach is:
+Enter the number of rows : 6
+             1
+           1   1
+         1   2   1
+       1   3   3   1
+     1   4   6   4   1
+   1   5   10   10   5   1
+[Question.2](Iterative)
+The Pascle's Triangle using iterative approach is:
+Enter number of rows:7
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+                                       1  6  15  20  15  6  1                                       
+[Question.3]
+Enter Integer 1:5
+Enter Integer 2:8
+
+The quotient when 5 is divided by 8 is 0.
+The remainder when 5 is divided by 8 is 5.
+Que3.a
+Function is Not-callable
+Que3.b
+All result values are NOT 'non-zero'
+Que3.c
+Appended (4,5,6) in the values (0,5)
+Values greater than 4 is 5,5,6
+Que3.d
+Above result in set form is shown below:
+{5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+Que3.f
+Max value from set is 6
+Hash value of 6 is 6
+[Question.4]
+Hello, My name is Abhijeet and my roll no. is 21105116
+Destructor Called
+[Question.5]
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+Que.5b
+Employee Viren's data has been removed.
+[Question 6]
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:Abhijeet
+Enter Player2 name:Yash
+
+Enter Word by Abhijeet:Mango
+Enter Word by Yash:Fruit
+
+Friendship of Abhijeet and Yash is FAKE
